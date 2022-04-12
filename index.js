9086d33549a85f3decc5f873bebfa2d96821073b
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-11 19:48:35 -0700

    Exported lib files to index.js, created two functions (init, managerHire), question array.

--- index.js
+++ index.js
@@ -1,5 +1,76 @@
 const inquirer = require('inquirer');
 const fs = requirer('fs');
+const Engineer = require('./lib/Engineer');
+const Manager = requirer('./lib/Manager');
+const Intern = requirer('./lib/Intern');
 
 
-const questions = 
+function init(){
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'name',
+            choices:["Add Manager","Add Engineer","Add Intern","Exit App"],
+            message: 'Employer name?',
+        }]).then(function(response){
+            switch(response.name){
+                case "Add Manager":
+                    managerHire();
+                    break;
+                case "Add Enginerr":
+                    engineerHire();
+                    break;
+                case "Add Intern":
+                    inetrnHire();
+                    break;
+                default:
+                    generateHTML();
+            }
+
+        })
+}
+let question = [
+    {
+        type:"input",
+        message:"Enter Employee Name",
+        name:"empname"
+    },
+    {
+        type: "input",
+        massage: "Enter Employer Id",
+        name: "empid",
+    },
+    {
+        type: "input",
+        message: "Enter Employer Email",
+        name: "empemail"
+    }
+]
+
+let fileHTML = "";
+
+
+function managerHire() {
+    let managerQuestions = question;
+    managerQuestions.push({
+        type:"input",
+        message:"Enter Manager Office Number",
+        name:"manofficenum",
+    })
+    inquirer.prompt(managerQuestions).then(function(response){
+        let theManager = new Manager(response.empname.response.empid.response.empemail.response.manofficenum);
+        fileHTML += `
+        <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Name: ${theManager.name}</h6>
+    <p class="card-text">Id: ${theManager.id}</p>
+    <a href="mailto:${theManager.email}" class="card-link"></a>
+    <p>Office Number: ${theManager.officeNumber}</p>
+  </div>
+</div>
+        `
+    })
+}
+

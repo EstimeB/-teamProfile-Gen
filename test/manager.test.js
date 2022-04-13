3bcde120d468ce0ab9dbada8c8fe594d48afab95
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-12 21:06:57 -0700

    added testing codes under manager.test.js.

---
+++ test/manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require('../lib/manager');
+const manager = new Manager('Testname', '2345678', 'testname@yahoo.com', 'fr234567');
+
+it('Should test for the constructor values', () => {
+    expect(manager.name).toEqual('Testname');
+    expect(manager.id).toEqual('2345678');
+    expect(manager.email).toEqual('testname@yahoo.com');
+    expect(manager.officeNumber).toEqual('fr234567');
+});
+it('Should test for getName()', () => {
+    expect(manager.getName()).toEqual('Testname');
+});
+it('Should test for getId()', () => {
+    expect(manager.getId()).toEqual('2345678');
+});
+it('Should test for getEmail()', () => {
+    expect(manager.getEmail()).toEqual('testname@yahoo.com');
+});
+it('Should test for getOfficeNumber()', () => {
+    expect(manager.getOfficeNumber()).toEqual('fr234567');
+});

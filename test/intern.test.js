bf80638ddd6c1d1baec91a1cf94d65123c23131b
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-12 20:58:19 -0700

    Few rewording under engineer.js and added testing codes for intern.test.js.

---
+++ test/intern.test.js
@@ -0,0 +1,21 @@
+const Intern = require('../lib/Intern');
+const intern = new Intern('Testname', '2345678', 'testname@yahoo.com', 'Schoolname');
+
+it('Should test for the constructor values', () => {
+    expect(intern.name).toEqual('Testname');
+    expect(intern.id).toEqual('2345678');
+    expect(intern.email).toEqual('testname@yahoo.com');
+    expect(intern.school).toEqual('Schoolname');
+});
+it('Should test for getName()', () => {
+    expect(intern.getName()).toEqual('Testname');
+});
+it('Should test for getId()', () => {
+    expect(intern.getId()).toEqual('2345678');
+});
+it('Should test for getEmail()', () => {
+    expect(intern.getEmail()).toEqual('testname@yahoo.com');
+});
+it('Should test for getschool()', () => {
+    expect(intern.getschool()).toEqual('Schoolname');
+});
